74b5e092ab8a383d2b179413d8f0a908806c9751
shubhamdots <shubhamsharma0888@gmail.com>
2023-02-03 14:57:18 +0530

    banner slider update

--- src/components/layouts/bannerSlide.tsx
+++ src/components/layouts/bannerSlide.tsx
@@ -1,25 +1,21 @@
-
-import { SplideSlide } from "@splidejs/react-splide";
 import * as React from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-import "../../assets/css/banner.css";
-const BannerSlide = (props:any) => {
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
-  const { imageForBanner} = props;  
-  const photos = imageForBanner?.map((element:any) => (   
-
-	<SplideSlide>
-    <img  src={element?.url} />
-	</SplideSlide>    
+const BannerSlide = (props: any) => {
+  // console.log('props=====', props)
+  const { imageForBanner, height, width } = props;
+  const photos = imageForBanner?.map((element: any) => (
+    <SplideSlide>
+      <img height={height} width={width} src={element?.url} />
+    </SplideSlide>
   ));
   return (
-    <Slide>
-      <div className="each-slide-effect">
-        <div>{photos} </div>
+    <>
+      <div className="">
+        <Splide>{photos}</Splide>
       </div>
-    
-    </Slide>
+    </>
   );
 };
 
--- src/components/locatorPage/SearchLayout.tsx
+++ src/components/locatorPage/SearchLayout.tsx
@@ -1,21 +1,28 @@
 import { useSearchActions } from "@yext/search-headless-react";
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from "react";
 import * as React from "react";
-import {  LocationBias, Pagination } from "@yext/search-ui-react";
+import { LocationBias, Pagination } from "@yext/search-ui-react";
 
 import { Location } from "../../types/search/locations";
 import LocationCard from "./LocationCard";
-import { AnswersHeadlessProvider } from '@yext/answers-headless-react';
+import { AnswersHeadlessProvider } from "@yext/answers-headless-react";
 import { GoogleMaps } from "./GoogleMaps";
 import { useSearchState, Result } from "@yext/search-headless-react";
 import Geocode from "react-geocode";
 import Address from "../commons/Address";
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import 'react-perfect-scrollbar/dist/css/styles.css';
+import PerfectScrollbar from "react-perfect-scrollbar";
+import "react-perfect-scrollbar/dist/css/styles.css";
 import $ from "jquery";
 import Banner from "../locationDetail/banner";
 import LoadingSpinner from "../commons/LoadingSpinner";
-import {breadcrumbhome, center_latitude, center_longitude, googleApikey, search_icn, UseMylocationsvg } from "../../../sites-global/global";
+import {
+  breadcrumbhome,
+  center_latitude,
+  center_longitude,
+  googleApikey,
+  search_icn,
+  UseMylocationsvg,
+} from "../../../sites-global/global";
 import { StaticData } from "../../../sites-global/staticData";
 
 import FilterSearch from "../locatorPage/FilterSearch";
@@ -26,7 +33,7 @@ import useFetchResults from "../../hooks/useFetchResults";
 import { Link } from "@mui/material";
 import { AnswerExperienceConfig } from "../../config/answersHeadlessConfig";
 
-var params1: any = { latitude: center_latitude, longitude:center_longitude }
+var params1: any = { latitude: center_latitude, longitude: center_longitude };
 var mapzoom = 8;
 var centerLatitude = center_latitude;
 var centerLongitude = center_longitude;
@@ -36,26 +43,26 @@ const SearchLayout = (props: any): JSX.Element => {
   type FilterHandle = React.ElementRef<typeof FilterSearch>;
   const filterRef = useRef<FilterHandle>(null);
   const locationResults = useFetchResults() || [];
-  const locationinbuit=useSearchState(state => state.vertical?.results) || [];
-  const alternateresult=useSearchState(state => state.vertical?.results?.length) || 0;
-  const[displaymsg,setDisplaymsg]=useState(false);
-  const [inputvalue, setInputValue] = React.useState('');
+  const locationinbuit =
+    useSearchState((state) => state.vertical?.results) || [];
+  const alternateresult =
+    useSearchState((state) => state.vertical?.results?.length) || 0;
+  const [displaymsg, setDisplaymsg] = useState(false);
+  const [inputvalue, setInputValue] = React.useState("");
   // const [inputvalue, setInputValue] = React.useState('');
-  const[allowlocation,setallowLocation]=React.useState('');
-  const[newparam,SetNewparam]=React.useState({latitude:0,
-    longitude:0});
+  const [allowlocation, setallowLocation] = React.useState("");
+  const [newparam, SetNewparam] = React.useState({ latitude: 0, longitude: 0 });
   const [offset, setOffset] = React.useState(0);
   const searchActions = useSearchActions();
-  const state = useSearchState(s => s) || [];
+  const state = useSearchState((s) => s) || [];
   const [optionclick, setOptionClick] = useState(true);
 
-const loading = useSearchState(s=>s.searchStatus.isLoading);
+  const loading = useSearchState((s) => s.searchStatus.isLoading);
 
   var searchKey: any;
   var target;
 
   var firstTimeRunners = true;
-
 
   const FirstLoad = () => {
     setCheck(true);
@@ -94,153 +101,161 @@ const loading = useSearchState(s=>s.searchStatus.isLoading);
     }, 3100);
   };
   const onClick = () => {
+    if (navigator.geolocation) {
+      const error = (error: any) => {
+        if (error.code == 1) {
+          setallowLocation("Please allow your Location");
+        }
+      };
 
-   if (navigator.geolocation) {
-    const error =(error:any) => {    
-     
-      if(error.code == 1){
-        setallowLocation('Please allow your Location')
-          
-          }       
-  };
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          Geocode.setApiKey(googleApikey);
+          var inputformat = "";
+          Geocode.fromLatLng(
+            position.coords.latitude,
+            position.coords.longitude
+          ).then(
+            (response: any) => {
+              if (response.results[0]) {
+                filterRef.current &&
+                  filterRef.current.setInputValue(
+                    response.results[0].formatted_address
+                  );
+                setallowLocation("");
+              }
+            },
+            (error: any) => {
+              console.error(error);
+              setCheck(false);
+            }
+          );
 
+          let params = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-      Geocode.setApiKey(googleApikey);
-      var inputformat='';
-      Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
-        (response: any) => {
-          if (response.results[0]) {
-            filterRef.current && filterRef.current.setInputValue(response.results[0].formatted_address);
-            setallowLocation('');
-          }
+          mapzoom = 3;
+          searchActions.setVertical("locations");
+          searchActions.setVerticalLimit(AnswerExperienceConfig.limit);
+          searchActions.setUserLocation(params);
+          searchActions.setOffset(0);
+          searchActions.executeVerticalQuery();
         },
-        (error: any) => {
-          console.error(error);
-          setCheck(false);
+        error,
+        {
+          timeout: 10000,
         }
       );
-
-      let params = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      };
-      
-      mapzoom=3;
-      searchActions.setVertical('locations');
-      searchActions.setVerticalLimit(AnswerExperienceConfig.limit)
-      searchActions.setUserLocation(params);
-      searchActions.setOffset(0);
-      searchActions.executeVerticalQuery();
-
-    },error, {
-      timeout: 10000,
-  });
-  }
-} 
-
-  
-  const Findinput = () => {
-    let searchKey = document.getElementsByClassName('FilterSearchInput');
-    let Search = (searchKey[0].value);
-    
-    setInputValue('');
-    if(searchKey[0].value!=""){
-    getCoordinates(Search);
     }
-    console.log(locationinbuit.length,"fisttimedispaly")
-    if(locationinbuit.length==0){
-      setDisplaymsg(true)
-    }else { 
+  };
+
+  const Findinput = () => {
+    let searchKey = document.getElementsByClassName("FilterSearchInput");
+    let Search = searchKey[0].value;
+
+    setInputValue("");
+    if (searchKey[0].value != "") {
+      getCoordinates(Search);
+    }
+    console.log(locationinbuit.length, "fisttimedispaly");
+    if (locationinbuit.length == 0) {
+      setDisplaymsg(true);
+    } else {
       setDisplaymsg(false);
     }
-  }
+  };
 
   const handleInputValue = () => {
-    setInputValue('');
-  }
-  const handleSetUserShareLocation = (value:any, userShareStatus:boolean) => {
-    console.log(value,center_latitude,center_longitude,"value");
+    setInputValue("");
+  };
+  const handleSetUserShareLocation = (value: any, userShareStatus: boolean) => {
+    console.log(value, center_latitude, center_longitude, "value");
     setInputValue(value);
-    if(userShareStatus){
+    if (userShareStatus) {
       setCenterLatitude(center_latitude);
       setCenterLongitude(center_longitude);
     }
+  };
+
+  function getCoordinates(address: String) {
+    setInputValue("");
+
+    setCheck(true);
+    // console.log(searchActions,"searchActions")
+    searchActions.setQuery(address);
+    searchActions.setUserLocation(params1);
+    searchActions.setOffset(0);
+    searchActions.executeVerticalQuery();
   }
 
+  let bannerimage =
+    props._site.c_locatorBannerImage != undefined
+      ? props._site.c_locatorBannerImage.image.url
+      : "";
 
-    function getCoordinates(address: String) {
-    setInputValue('');
-   
- 
-          setCheck(true);
-          // console.log(searchActions,"searchActions")
-          searchActions.setQuery(address);
-          searchActions.setUserLocation(params1);
-          searchActions.setOffset(0);
-          searchActions.executeVerticalQuery();
-          
-  }
-
-  let bannerimage = props._site.c_locatorBannerImage != undefined  ? props._site.c_locatorBannerImage.image.url:'';
- 
-
-  // const loader =
-  //   isLoading ? <LoadingSpinner /> : '';
+  const loader = isLoading ? <LoadingSpinner /> : "";
 
   const addClass = () => {
-
     document.body.setAttribute("class", "mapView");
     // setActive('')
+  };
 
-
-  }
-
-  useEffect(()=>{
-    if(locationinbuit.length>0){
+  useEffect(() => {
+    if (locationinbuit.length > 0) {
       setDisplaymsg(false);
     }
-    },[locationinbuit])
-    useEffect(()=>{
-      console.log("yes rerender")
-      locationResults.map((result:any, index:number) => {
-        const resultelement = document.querySelectorAll(
-          `.result-list-inner-${index + 1}`
-        );
-        for (let index = 0; index < resultelement.length; index++) {
-          if(resultelement[index].classList.contains("active")||resultelement[index].classList.contains("fixed-hover")){
+  }, [locationinbuit]);
+  useEffect(() => {
+    // console.log("yes rerender");
+    locationResults.map((result: any, index: number) => {
+      const resultelement = document.querySelectorAll(
+        `.result-list-inner-${index + 1}`
+      );
+      for (let index = 0; index < resultelement.length; index++) {
+        if (
+          resultelement[index].classList.contains("active") ||
+          resultelement[index].classList.contains("fixed-hover")
+        ) {
           resultelement[index].classList.remove("active", "fixed-hover");
         }
       }
-      });
-    },[loading])
- useEffect(()=>{
-  if (firstTimeRunners) {
-    firstTimeRunners = false;
-    // searchActions.resetFacets();
-    FirstLoad();
-  }
- },[])
+    });
+  }, [loading]);
+  useEffect(() => {
+    if (firstTimeRunners) {
+      firstTimeRunners = false;
+      // searchActions.resetFacets();
+      FirstLoad();
+    }
+    if (isLoading) {
+      $("body").addClass("overflow-hidden");
+    }
+  }, []);
 
   return (
     <>
-
-      {/* {loader} */}
+      {loader}
       <div className="breadcrumb">
         <div className="container-custom">
           <ul>
             <li>
-              <a href="#" className="home"> Home</a>
+              <a href="#" className="home">
+                {" "}
+                Home
+              </a>
             </li>
             <li>{StaticData.locator_breadcrumb}</li>
           </ul>
-
         </div>
       </div>
       <div className="locator-main">
-        {allowlocation.length > 0 ?
+        {allowlocation.length > 0 ? (
           <div className="for-allow">{allowlocation}</div>
-          : ''}
+        ) : (
+          ""
+        )}
         <div className="search-bx">
           <div className="location-with-filter">
             <h1 className="">{StaticData.FindLocationtext}</h1>
@@ -248,13 +263,13 @@ const loading = useSearchState(s=>s.searchStatus.isLoading);
 
           <div className="search-field">
             <FilterSearch
-             ref={filterRef}
-             displaymsg={displaymsg}
-             setDisplaymsg={setDisplaymsg}
+              ref={filterRef}
+              displaymsg={displaymsg}
+              setDisplaymsg={setDisplaymsg}
               customCssClasses={{
                 filterSearchContainer: "m-2 w-full",
                 inputElement: "FilterSearchInput pr-[90px]",
-                optionsContainer: "options"
+                optionsContainer: "options",
               }}
               inputvalue={inputvalue}
               setSearchInputValue={setInputValue}
@@ -264,67 +279,86 @@ const loading = useSearchState(s=>s.searchStatus.isLoading);
                 {
                   entityType: "location",
                   fieldApiName: "address.line1",
-
                 },
                 {
                   entityType: "location",
                   fieldApiName: "address.postalCode",
-
-                  },
-                  {
-                    entityType: "location",
-                    fieldApiName: "name",
-
+                },
+                {
+                  entityType: "location",
+                  fieldApiName: "name",
                 },
                 {
                   entityType: "location",
                   fieldApiName: "address.city",
-
                 },
                 {
                   entityType: "location",
                   fieldApiName: "address.region",
+                },
+                // {
+                //   entityType: "location",
+                //   fieldApiName: "address.countryCode",
 
-                  },
-                  // {
-                  //   entityType: "location",
-                  //   fieldApiName: "address.countryCode",
-
-                  // },
-                ]}
-                
-                handleInputValue={handleInputValue}  
-                handleSetUserShareLocation={handleSetUserShareLocation}
+                // },
+              ]}
+              handleInputValue={handleInputValue}
+              handleSetUserShareLocation={handleSetUserShareLocation}
             />
 
             <button
               className="search-btn"
               aria-label="Search bar icon"
-              id="search-location-button" onClick={Findinput}>
-                <span dangerouslySetInnerHTML={{ __html: search_icn }} />
-                </button>
+              id="search-location-button"
+              onClick={Findinput}
+            >
+              <span dangerouslySetInnerHTML={{ __html: search_icn }} />
+            </button>
           </div>
 
-<div className="fliter-sec">
-          <button className="useMyLocation" title="Search using your current location!" id="useLocation" onClick={onClick}>
-              <span className="icon" dangerouslySetInnerHTML={{ __html: UseMylocationsvg }} />
+          <div className="fliter-sec">
+            <button
+              className="useMyLocation"
+              title="Search using your current location!"
+              id="useLocation"
+              onClick={onClick}
+            >
+              <span
+                className="icon"
+                dangerouslySetInnerHTML={{ __html: UseMylocationsvg }}
+              />
 
-             <span className="underline hover:no-underline"> {StaticData.Usemylocation}</span>
+              <span className="underline hover:no-underline">
+                {" "}
+                {StaticData.Usemylocation}
+              </span>
             </button>
 
-          <ResultsCount
-            customCssClasses={{ container: "mx-2 my-0 text-dark-gray" }}
-          />
-</div>
+            <ResultsCount
+              customCssClasses={{ container: "mx-2 my-0 text-dark-gray" }}
+            />
+          </div>
         </div>
         <div className="mobile-btns">
           <div className="button-bx">
-
-            <a className="btn listBtn" href="javascript:void(0);" onClick={() => {
-              document.body.classList.remove('mapView')
-
-            }}> List View</a>
-            <a className="btn mapBtn" href="javascript:void(0);" onClick={addClass}> Map View</a>
+            <a
+              className="btn listBtn"
+              href="javascript:void(0);"
+              onClick={() => {
+                document.body.classList.remove("mapView");
+              }}
+            >
+              {" "}
+              List View
+            </a>
+            <a
+              className="btn mapBtn"
+              href="javascript:void(0);"
+              onClick={addClass}
+            >
+              {" "}
+              Map View
+            </a>
           </div>
         </div>
         <div className=" map-section ">
@@ -339,43 +373,47 @@ const loading = useSearchState(s=>s.searchStatus.isLoading);
         </div>
 
         <div className="left-listing">
-
-          <PerfectScrollbar >
-
+          <PerfectScrollbar>
             <div>
-             
-                <VerticalResults
-                  displayAllOnNoResults={false}
-                  CardComponent={LocationCard}
-                  locationResults={locationinbuit}
-                  customCssClasses={{
-                    container:
-                      "result-list flex flex-col scroll-smooth  overflow-auto",
+              <VerticalResults
+                displayAllOnNoResults={false}
+                CardComponent={LocationCard}
+                locationResults={locationinbuit}
+                customCssClasses={{
+                  container:
+                    "result-list flex flex-col scroll-smooth  overflow-auto",
+                }}
+                // CardComponent={LocationCard}
+              />
 
-                  }}
-                  // CardComponent={LocationCard}
+              {locationinbuit && locationinbuit.length <= 0 ? (
+                <div className="browse-dir">
+                  <a className="underline " href="/gb.html">
+                    Use the search above or{" "}
+                    <span className="font-second-main-font">
+                      {" "}
+                      browse our directory
+                    </span>
+                  </a>
+                </div>
+              ) : (
+                ""
+              )}
+              <div className="button-bx">
+                <ViewMore
+                  className={
+                    " btn notHighlight lg:!w-[132%] !mb-2 button view-more"
+                  }
+                  idName={"view-more-button"}
+                  buttonLabel={"View More"}
                 />
-             
-      
-              {locationinbuit && locationinbuit.length <= 0 ?
-               <div className="browse-dir">
-               <a className="underline " href='/gb.html'>Use the search above or <span className="font-second-main-font"> browse our directory</span></a> 
-               </div>:''}
-                <div className="button-bx">
-               <ViewMore  className={" btn notHighlight lg:!w-[132%] !mb-2 button view-more"} idName={"view-more-button"} buttonLabel={"View More"} />
-               </div>
+              </div>
             </div>
           </PerfectScrollbar>
         </div>
-
-
       </div>
-
-
     </>
   );
 };
 
 export default SearchLayout;
-
-
